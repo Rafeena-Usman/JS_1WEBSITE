17b187d3befc92267ff2194fd8bbb24abae84d69
Rafeena-Usman <123816266+Rafeena-Usman@users.noreply.github.com>
2023-04-04 14:52:50 +0530

    JS Website one

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    var MenuToggle= document.querySelector('.menutoggle');
+    var Nav_Elements= document.querySelector('.nav_elements');
+    MenuToggle.classList.toggle('active');
+    Nav_Elements.classList.toggle('active');
+
+}
